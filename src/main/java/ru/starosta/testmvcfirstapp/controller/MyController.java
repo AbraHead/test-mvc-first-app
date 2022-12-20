3714e0f057c76e8b5e1926cae472fc7918db3217
Виталий Вишневский (Infloyd) <v.vishnevskiy@infloyd.ru>
2022-12-20 14:44:59 +0500

    Add html template and move MyController

--- src/main/java/ru/starosta/testmvcfirstapp/controller/MyController.java
+++ src/main/java/ru/starosta/testmvcfirstapp/controller/MyController.java
@@ -1,4 +1,4 @@
-package ru.starosta.testmvcfirstapp;
+package ru.starosta.testmvcfirstapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
